a76cad037247884db6044985f141d0c57e81cf8d
Rana Gahwagy <rgahwagy@smith.edu>
2022-02-15 13:42:35 -0500

    making differnt graphs for data

--- Code/02_createModelScript.R
+++ Code/02_createModelScript.R
@@ -4,110 +4,109 @@ library(tidyverse)
 library(scales)
 library(readr)
 #Load Absorbance Data----
-  #Read in data containing wet chem BSi percentages 
+  #Read in data containing wet chem BSi percentages
   wetChemAbsorbance <- read_csv("csvFiles/wetChemAbsorbance.csv") ### 28 3699
-  
-  #Remove sample names 
+
+  #Remove sample names
   wetChemAbsorbance <- wetChemAbsorbance %>%
     select(-1)
   dim(wetChemAbsorbance) ## 28 3698
-  
+
 #Code for PLS model----
-  
+
     ##Select CV based on number of samples
-    
+
     ### Cross-Validation is 5-fold ###
     plsModel <- plsr(BSiPercent~., ncomp = 10, data=wetChemAbsorbance, validation = "CV", segments = 5)
-  
+
   ###There is a predict function in the pls package to predict BSi percentages for data without wet chem BSi percentages
  # predict(plsModel, ncomp = 1:10, newdata = newData)
-  
+
     ### Summary of Cross Validation and number of components
     print(summary(plsModel))
 #Root Mean Squared Error Plots----
-  
+
       #Plot RMSEP
       plot(RMSEP(plsModel))
-    
+
       ## save root mean squared error of prediction to object res
       RMSEP <- RMSEP(plsModel)
-      
+
       #Dataframe with cross validation and number of components
       Five_fold_CV <- cbind.data.frame(cv = RMSEP$val[1,,], ncomps = 0:10)
-      
-      #Number of components graph 
+
+      #Number of components graph
       ggplot(Five_fold_CV, aes(ncomps, cv)) +
         geom_line() +
-        labs(title = "Cross-validated Root Mean Squared Error of Prediction (RMSEP) Curve", 
+        labs(title = "Cross-validated Root Mean Squared Error of Prediction (RMSEP) Curve",
              subtitle="CV is 5-fold",
-             y = "RMSEP", 
+             y = "RMSEP",
              x = "Number of Components") +
-        scale_x_continuous(breaks = c(1:10)) + 
+        scale_x_continuous(breaks = c(1:10)) +
         theme_minimal()
-      
+
 #Create "test data" with different loadings ----
-  
-    ## Load Opus txt file 
+
+    ## Load Opus txt file
        # reading in list of txt files and converting it to a df
-        
+
       ## read in txt files automatically
       fname <- list.files("Samples/greenlandSamples", full.names = T)
-      
+
       ##FUNCTION 1: Add Sample Names
       addSampleNames <- function(fname) {
         ## creates list of txt files
         filelist <- lapply(fname, read.delim, header = F)
-        
+
         ## Adding sample IDs (reference to lake core)
         names(filelist) <- gsub(".*/(.*)\\..*", "\\1", fname)
-        
+
         return(filelist)
       }
-      
+
       #Save list of txt files with sample names
       filelist <- addSampleNames(fname)
-      
+
       # save the transformed df to a new list of df called reformattedData
       reformattedData <- lapply(filelist, function(x) {
         pivot_wider(x, names_from = V1, values_from = V2)
       })
-      
+
       # Unlist the reformattedData list into matrix
-      wavenumber_matrix <- lapply(reformattedData, names) 
-      
+      wavenumber_matrix <- lapply(reformattedData, names)
+
       # convert matrix into dataframe
       wavenumber_df <- as.data.frame(do.call("rbind",wavenumber_matrix))
-      
+
       wavenumber <- as.numeric(as.vector(unname(t(wavenumber_df[1,])))) ### 1 3697
-  
-      
+
+
 #Data frame for different loadings
       #loadings, #first column of random opus txt file)
-      loadingPlotData<- cbind.data.frame(loadings_1 = plsModel$loadings[,1], 
-                                  loadings_2= plsModel$loadings[,2], 
-                                  loadings_3 = plsModel$loadings[,3], 
-                                  weighted_loading_1 = plsModel$loading.weights[,1], 
-                                  weighted_loading_2 = plsModel$loading.weights[,2], 
-                                  weighted_loading_3 = plsModel$loading.weights[,3], 
+      loadingPlotData<- cbind.data.frame(loadings_1 = plsModel$loadings[,1],
+                                  loadings_2= plsModel$loadings[,2],
+                                  loadings_3 = plsModel$loadings[,3],
+                                  weighted_loading_1 = plsModel$loading.weights[,1],
+                                  weighted_loading_2 = plsModel$loading.weights[,2],
+                                  weighted_loading_3 = plsModel$loading.weights[,3],
                                   wavenumber = wavenumber)
-      
+
 #Loading Plots----
- 
+
          #Loading plot for Full Spectrum loading 1 with legend and vertical lines
-         
-         ggplot(loadingPlotData, aes(x=wavenumber)) + 
+
+         ggplot(loadingPlotData, aes(x=wavenumber)) +
          geom_line (aes(y = weighted_loading_1, colour = "1st Component")) +
-         geom_line (aes(y = weighted_loading_2, color = "2nd Component")) + 
-         geom_line (aes(y = weighted_loading_3, colour = "3rd Component")) + 
-         
-         scale_colour_manual("", 
+         geom_line (aes(y = weighted_loading_2, color = "2nd Component")) +
+         geom_line (aes(y = weighted_loading_3, colour = "3rd Component")) +
+
+         scale_colour_manual("",
                              breaks = c("1st Component", "2nd Component", "3rd Component"),
                              values = c("blue", "dark green", "orange")) +
-         
-         labs(y="Weighted Loadings", 
-              x=expression(Wavenumber(cm^-1)), 
+
+         labs(y="Weighted Loadings",
+              x=expression(Wavenumber(cm^-1)),
               title='Loading Plot for Three Components: Full Spectrum, n = 28',
               subtitle= expression("Where wavenumber ranges from ~7500 to ~370" ~ cm^{-1}))+
-         scale_x_reverse() + 
+         scale_x_reverse() +
          theme_minimal()
-      
---
+++ rana_testing.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+library(Metrics)
+
+#read in greenland samples
+fname <- list.files("Samples/greenlandSamples", full.names = T)
+filelist <- lapply(fname, read.table, sep="")
+names(filelist) <- gsub(".*/(.*)\\..*", "\\1", fname)
+
+# plotting all of the data sprectrum
+par(mfrow = c(6, 5))
+par(mar=c(1,1,1,1))
+
+for (i in 1:length(filelist))
+{
+  plot(filelist[[i]])
+} ## the last two seem off
+
+#reformat data
+
+#intopolation
+
+#create model (assuming vivienne;s code is all run)
+plsModel <- plsr(BSiPercent~., ncomp = 10, data=wetChemAbsorbance, validation = "CV", segments = 5)
+summary(plsModel)
+RMSEP <- RMSEP(plsModel)
+par(mfrow = c(1, 1))
+plot(RMSEP)
+
+plot(plsModel, ncomp = 3:8, asp = 1, line = TRUE) # there are some outliers,  best # 3,4,5
+#score plots: are often used to look for patterns, groups or outliers in the data.
+plot(plsModel, plottype = "scores", comps = 3:5)
+#explained vaiance
+explvar(plsModel)
+#loading plot: interpretation purposes, for instance to look for known spectral peaks or profiles:
+plot(plsModel, "loadings", comps = 2:8, legendpos = "topleft", xlab = "nm")
+abline(h = 0)
+
+
+
+  for (i in 2:10){
+    plsModel <- plsr(BSiPercent~., ncomp = i, data=wetChemAbsorbance, validation = "CV", segments = 5)
+
+    predicted_bsi <- as.data.frame(plsModel$fitted.values)
+    #select model with 3 components
+    predicted_bsi <- as.data.frame(predicted_bsi[,c(i)])
+    #Rename wet_chem_data columns
+    names(predicted_bsi )[1] <- "BSiPercent_Predicted"
+    #Combine actual and predicted BSi wetchem data
+    BSi <- cbind(actual_bsi_wetchem, predicted_bsi)
+    #reformat into long so we can graph
+    BSi_Long <- BSi %>%
+      select(dataset, BSiPercent, BSiPercent_Predicted)%>%
+      gather(key = "variable", value = "value", -dataset)
+    #Create dataframe with residual error ----
+    #Calculating Residuals: Difference between actual and predicted
+    BSi$Difference <- (BSi$BSiPercent_Predicted - BSi$BSiPercent)
+    #Table of differences for each sample
+    Difference <- BSi %>%
+      select(dataset, Difference)
+    Difference <- round_df(Difference, 2)
+    #Calculate absolute values, mean and median of residual errors ----
+    Abs <- (abs(Difference$Difference))
+
+    print(Difference %>%
+      mutate(highlight_flag = ifelse(Difference >= '0', T, F)) %>%
+      ggplot (aes(x = dataset, y = Difference)) +
+      geom_col(aes(fill = highlight_flag)) +
+      scale_fill_manual(values = c('red', 'green'), name = "Overfitting") +
+      geom_text( data = Difference, aes(label = Difference), fontface ="bold", size = 2.5, vjust = 0) +
+      labs(
+        y = "Difference in Percentage",
+        x = "Sample ID",
+        title=expression("Residuals: Full Spectrum" ~ cm^{-1}),
+        subtitle= paste0("For 28 Samples with ", i, " comp and ", 5, "segments"),
+        colour = "variabl") +
+      theme(legend.position = c(0.1, 0.85),
+            axis.text.x  = element_text(angle = 90)) )
+  }
+
