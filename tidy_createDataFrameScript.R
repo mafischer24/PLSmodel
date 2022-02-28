619bf62c08282587df6e56728cb8145270a03ae7
Grace Hartley <grace.hartley@gmail.com>
2022-02-27 21:25:13 -0500

    added full script

--- tidy_createDataFrameScript.R
+++ tidy_createDataFrameScript.R
@@ -74,7 +74,8 @@ create_absorbance_df <- function(x){
   })
 
   # This is Vivienne's function dropNames
-
+  # Don't know if we need the following line
+  #wavenumber_df$dataset <- names(filelist) ## make this a specific column, don't trust it to store
   # Rename column header from "wavenumbers" to "Vi" (FUNCTION #3)
   dropNames <- function(data) {
     names(data) <- paste("V", 1:ncol(data), sep = "")
@@ -87,49 +88,13 @@ create_absorbance_df <- function(x){
 
 }
 
+# Write to .csv in example file, should it write to the same .csv every time?
+# Not sure if we need to have a column with sample names like in Vivienne's code? That might be missing if we have bugs later
 
-write.csv(wavenumber_df, "csvFiles/wavenumber.csv")
-write.csv(absorbance_df, "csvFiles/absorbance.csv")
+# Should this be in script or in example file?
 
-# _____________________
+# Below here is unchanged from Vivienne's code. Can be streamlined I think
 
-
-dropNames <- function(data) {
-  names(data) <- paste("V", 1:ncol(data), sep = "")
-  return(data)
-}
-
-
-# creating new list of df where there aren't any wavenumbers...only absorbance values [1:3697]
-absorbance_matrix <- lapply(reformattedData, dropNames)
-
-# Dataframe of [28:3697]where absorbance values are in cells
-## need to resolve mismatch in wavenumbers before moving forward
-absorbance_df <- do.call(rbind.data.frame, absorbance_matrix)
-
-lapply(reformattedData, ncol) %>%
-  unlist() %>%
-  summary()
-
-# checking summary
-# lapply(reformattedData, ncol) %>% unlist() %>% summary()
-# which are not 1882
-# which(unlist(lapply(reformattedData, ncol)) != 1882)
-### AW-34.5 (8_31_16).0  AW-7.5 (8_31_16).0   AW-73 (8_31_16).0
-
-## adds column for each row to remind us which file it is
-absorbance_df$dataset <- names(filelist)
-
-## Make data sample name in first column
-wavenumber <- wavenumber_df[, c(ncol(wavenumber_df), 1:(ncol(wavenumber_df) - 1))] ### 28:3698
-
-absorbance <- absorbance_df[, c(ncol(absorbance_df), 1:(ncol(absorbance_df) - 1))] ### 28:3698
-
-# write csv
-write.csv(wavenumber, "csvFiles/wavenumber.csv")
-write.csv(absorbance, "csvFiles/absorbance.csv")
-
-### Components of Function 4-----
 # Read in calibration csv with same number of samples as our transformedData
 wet_chem_data <- read_csv("csvFiles/wet-chem-data.csv") ### 28
 
