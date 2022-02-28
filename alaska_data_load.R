0afd097f3597b94946d71fd493e20c3d33a39ea9
Rana Gahwagy <rgahwagy@smith.edu>
2022-02-28 13:28:23 -0500

    adding grabbing the names for the alaska samples

--- alaska_data_load.R
+++ alaska_data_load.R
@@ -1,22 +1,31 @@
 library(tidyverse)
+#upload alaska files
+afname <- list.files("Samples/alaska_csv", full.names = T)
+alaska_filelist <- lapply(afname, read.table, sep=",", header=TRUE)
 
-afname <- list.files("Samples/alaskaSamples", full.names = T)
-alaska_filelist <- lapply(afname, read.table, sep="")
-
-#how to make a regular exp to extract names of samples?
-names(alaska_filelist) <- grep(".*/(.*)\\..*", afname) ## not sure what that mean but it numbers samples
+#grab names
+#grab file name
+anames <- list.files("Samples/alaska_csv", full.names = F)
+#split by space: makes a list of list of names
+anames <-  str_split(anames,pattern = " " )
+#create a variable to hold names
+alaska_sample_names <- numeric(length = length(anames))
+#populate the variables
+for(i in 1:length(anames)){
+  alaska_sample_names[i]<- anames[[i]][1]
+}
 
 #attach names to column
+names(alaska_filelist) <- alaska_sample_names
 
-##the data has one column, I am assuming it is wavenumber and absorbance values
-## why values are a factor? need to reformat
-
+##need to reformat be removing the first column of numbers + change absorbance
 
 #make a wave number matrix + absorbance matrix
 for(i in 1:100){
-  alaska_samples <- alaska_filelist[[i]] %>%
-    ## seperating into two columns
-    separate(col = 1, into = c("wavenumber", "absorbance"), sep = ",")
+
+  alaska_filelist[[i]] %>%
+    select(-1)
+
   #pivot wider
   # add each row to respective matrix
 }
