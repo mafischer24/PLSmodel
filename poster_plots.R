c66c680b967d201d0f7c6830f946741b75999dfc
Rana Gahwagy <rgahwagy@smith.edu>
2022-03-02 11:17:51 -0500

    Tried to fix the read_file() function and I think it worked, I think the problem was in seperate (it only works on chr). I have in additional_function.R line 57, and aaded the two create_data_frame function too.

--- poster_plots.R
+++ poster_plots.R
@@ -5,17 +5,34 @@ library(pls)
 #quartz vs diatoms vs lake sample spectra ( what kind of data are we looking at? )
 # what is ss?  sea sand
 SS <- reformattedData$`SS.0` %>%
-  pivot_longer(everything(), names_to = "wavenumber", values_to = "absorbance")
+  pivot_longer(everything(), names_to = "wavenumber", values_to = "absorbance") %>%
+  mutate(wavenumber = as.numeric(wavenumber))
 
 WQ <- reformattedData$`WQ.0` %>%
-  pivot_longer(everything(), names_to = "wavenumber", values_to = "absorbance")
+  pivot_longer(everything(), names_to = "wavenumber", values_to = "absorbance")%>%
+  mutate(wavenumber = as.numeric(wavenumber))
+
+DB <- read.table("~/Downloads/DB 7 [2].0.dpt")
+names(DB)[1] <- "wavenumber"
+names(DB)[2] <- "absorbance"
+DB <- DB %>%
+  mutate(wavenumber = as.numeric(wavenumber))
 
 # where is the diatom data
 #random lake sample
 
+#find areas for BSI to highlight
+intervals <- c(368, 3750, 435, 480, 790, 830,1050, 1280)
+WQ %>% filter_all(any_vars(. %in% intervals))
+which(WQ == contains(intervals))
+
 ggplot() +
   geom_line(data = WQ, aes(x=as.numeric(wavenumber), y= absorbance, color = "blue"))+
-  geom_line(data = SS, aes(x=as.numeric(wavenumber), y= absorbance))
+  geom_line(data = DB, aes(x=as.numeric(wavenumber), y= absorbance)) +
+  scale_x_reverse() +
+  theme_bw() +
+  geom_rect(data=rects, inherit.aes=FALSE, aes(xmin=start, xmax=end, ymin=min(dat$value),
+                                               ymax=max(dat$value), group=group), color="transparent", fill="orange", alpha=0.3)
 
 
 
@@ -36,6 +53,8 @@ ggplot(pls_bsi, aes(x=wet_chem, y = pls)) +
   ylab("PLS Predicted BSi Percent (%)")+
   ggtitle("Predeicted Fitness Using PLS Model")+
   theme_bw()
+
+
 
 
 
