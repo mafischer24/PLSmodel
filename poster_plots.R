ed2a2f28ddb7ecf417a45b58a59b6c4926cb72dd
Rana Gahwagy <rgahwagy@smith.edu>
2022-03-07 11:02:16 -0500

    making poster plots

--- poster_plots.R
+++ poster_plots.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(Metrics)
 library(pls)
+library(tidyr)
+library(ggpubr)
 
 #quartz vs diatoms vs lake sample spectra ( what kind of data are we looking at? )
 # what is ss?  sea sand
@@ -10,12 +12,35 @@ SS <- reformattedData$`SS.0` %>%
 WQ <- reformattedData$`WQ.0` %>%
   pivot_longer(everything(), names_to = "wavenumber", values_to = "absorbance")
 
+DB <- read.table("~/Downloads/DB 7 [2].0 (1).dpt", sep = " ") %>%
+  separate(col = "V1", into = c("wavenumber", "absorbance"),
+           convert = TRUE, sep = "[[:space:]]")
+
+DE <- read.table("~/Downloads/DE 2.0.dpt", sep = " ") %>%
+  separate(col = "V1", into = c("wavenumber", "absorbance"),
+           convert = TRUE, sep = "[[:space:]]")
 # where is the diatom data
 #random lake sample
+#435 - 480cm-1
+#790 - 830cm-1
+#1050 - 1280cm-1
+
+start <- c(435, 790, 1050)
+end <- c(480, 830, 1280)
+
 
 ggplot() +
-  geom_line(data = WQ, aes(x=as.numeric(wavenumber), y= absorbance, color = "blue"))+
-  geom_line(data = SS, aes(x=as.numeric(wavenumber), y= absorbance))
+  geom_line(data = WQ, aes(x=as.numeric(wavenumber), y= absorbance, color = "Washed quartz"), color = "black")+
+  geom_line(data = DB, aes(x=as.numeric(wavenumber), y= absorbance, color = "High BSi Sample"), color = "blue") +
+  geom_line(data = DE, aes(x=as.numeric(wavenumber), y= absorbance), color = "darkred") +
+  theme_bw() +
+  xlim(4000, 300)+
+  xlab("Absorbance") +
+  ylab(expression(paste("Wavenumber ", cm^{-1}))) +
+  geom_rect(inherit.aes=FALSE, aes(xmin=start, xmax=end, ymin= - Inf, ymax=Inf),
+            color="transparent", fill="orange", alpha=0.3) +
+  ggtitle("Example FTIRS Spectra")
+
 
 
 
