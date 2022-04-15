b6afa4d690f6b7fbc8e5ed18717e3df37b0e09d1
Grace Hartley <grace.hartley@gmail.com>
2022-04-15 17:50:10 -0400

    adding shiny stuff

--- Shiny/app.R
+++ Shiny/app.R
@@ -1,14 +1,37 @@
 library(shiny)
 library(tidyverse)
 
+# Need to attach our df
+#replace this line with the functions eventually (or the attached
+# dataset!)
+
+
 # create the user interface
 ui <- fluidPage(
-  "This is our PLS Shiny App"
+  "This is our PLS Shiny App",
+  selectInput("whichdatachoose",
+              "Which datasets would you like to use as calibration data?",
+              whichdata, multiple = TRUE),
+  fileInput("upload", "Select your FTIRS dataset", accept = ".csv"),
+  tableOutput("contents")
 )
 
 # define the behavior of app
 server <- function(input, output, session){
+  output$contents <- renderTable({
+    file <- input$upload
+    ext <- tools::file_ext(file$datapath)
+    req(file)
+    validate(need(ext == "csv", "Please upload a csv file"))
+    read_csv(file$datapath)
+  })
 
+  pls_data <- reactive({
+    source('R/compiled_data_load.R')
+  })
+  output$summary <- renderPrint({
+    summary(pls_data())
+  })
 }
 
 # build shiny app
