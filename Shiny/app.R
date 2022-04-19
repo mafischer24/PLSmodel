17426e15ab9838b7e4278a8e60c37c7bd399f108
Rana Gahwagy <rgahwagy@smith.edu>
2022-04-18 21:22:19 -0400

    testing r Shiney

--- Shiny/app.R
+++ Shiny/app.R
@@ -1,15 +1,37 @@
 library(shiny)
-library(tidyverse)
+library(ggplot2)
+library(pls)
 
-# create the user interface
-ui <- fluidPage(
-  "This is our PLS Shiny App"
+intro_panel <- tabPanel(
+  "About",
+  titlePanel("Learn about our model"),
+  img(src = "~/Desktop/PLSmodel/paper_files/final_paper_draft/fig1.png"),
+  p("we predict BSi and TOC content from FTIRS data")
 )
 
-# define the behavior of app
-server <- function(input, output, session){
+locations <- c("Alaska", "Greenland", "Arctic (AK+GL)")
 
+second_panel <- tabPanel(
+  "use Model",
+  titlePanel("upload your data"),
+  selectInput("dataset", label ="what is your location?", choices = locations ),
+  fileInput("upload", "Upload a file"),
+  tableOutput("files")
+
+)
+
+rmsep_plot <- mainPanel(
+  plotOutput("plot")
+)
+
+ui <- navbarPage(
+  "BSi Predictive Model",
+  intro_panel,
+  second_panel
+)
+
+server <- function(input, output, session) {
+  output$files <- renderTable(input$upload)
 }
 
-# build shiny app
 shinyApp(ui, server)
