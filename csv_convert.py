5469eff19e2a0b05b522067dcb63698a4822a7bd
Molly Fischer <mafischer@smith.edu>
2022-06-29 13:28:21 -0400

    Rewrote csv_convert.py file as a .Rmd file. This code converts .dpt files to .csv files.

--- csv_convert.py
+++ csv_convert.py
@@ -3,8 +3,11 @@ import pandas as pd
 from os import listdir
 
 # Get the lists of files in both directories
+# How to replicate in R? 
+# How to do this automically? One function?
 alaska_files = listdir("Samples/alaskaSamples")
 greenland_files = listdir("Samples/greenlandSamples")
+lacawac_files = listdir("Samples/LWSamples")
 
 for file in alaska_files:
     # Read in the .dpt file with the correct column names
@@ -21,3 +24,10 @@ for file in greenland_files:
     file = file.replace(".txt",".csv")
     # Write to csv format
     file_pd.to_csv("Samples/greenland_csv/"+file)
+    
+for file in LWSamples:
+# Read in the .dpt file with the correct column names
+    file_pd = pd.read_csv("Samples/LWSamples/" + file, sep = ',', names = ['wavenumber','absorbance'])
+    file = file.replace(".dpt",".csv")
+    # Write to csv format
+    file_pd.to_csv("Samples/LW_csv/" + file)
